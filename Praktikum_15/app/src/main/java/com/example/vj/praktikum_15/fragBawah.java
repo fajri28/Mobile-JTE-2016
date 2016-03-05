c13ed7906471343a1a6425938a1c8ced667dc1f9
fajri28 <fajri28mhd@gmail.com>
2016-03-05 21:50:22 +0800

    fragment-button

---
+++ Praktikum_15/app/src/main/java/com/example/vj/praktikum_15/fragBawah.java
@@ -0,0 +1,22 @@
+package com.example.vj.praktikum_15;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class fragBawah extends Fragment {
+
+    public fragBawah() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bawah, container, false);
+    }
+}
