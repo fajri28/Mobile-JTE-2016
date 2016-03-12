c0464ca03c85533d6b8bf4d474b9732c965c5f3e
fajri28 <fajri28mhd@gmail.com>
2016-03-13 00:11:50 +0800

    tebak angka

---
+++ Praktikum18/app/src/main/java/com/example/vj/praktikum18/ActivityKedua.java
@@ -0,0 +1,31 @@
+package com.example.vj.praktikum18;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+/**
+ * Created by VJ on 3/12/2016.
+ */
+public class ActivityKedua extends Activity {
+    @Override
+    protected void onCreate(Bundle savedIntanceState) {
+
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_kedua);
+
+        Intent pesan = getIntent();
+        String pesanStr = pesan.getStringExtra("varl");
+
+        TextView txtLabel = (TextView) findViewById(R.id.textView);
+        txtLabel.setText(pesanStr);
+    }
+}
+
